3da35a45fc561b20710f0938e023d8574e39a906
Brigitte Hugonin <brigitte.hugonin@gmail.com>
2024-01-12 13:45:34 +0100

    prove

--- script.js
+++ script.js
@@ -17,3 +17,27 @@ const toggleMenu = () => {
 document.addEventListener('DOMContentLoaded', () => {
     toggle.addEventListener('click', toggleMenu)
 })
+
+
+
+
+//read more
+var nuovoTesto = document.querySelector('#DueColonne');
+var buttonMore = document.querySelector('#ButtonMore');
+
+
+
+function toggleReadMore() {
+    nuovoTesto.style.display = readMoreOpen ? 'none' : 'block';
+    if (nuovoTesto.style.display === 'none' || nuovoTesto.style.display === '') {
+        nuovoTesto.style.display = 'block';
+        buttonMore.textContent = 'Read less';
+    } else {
+        nuovoTesto.style.display = 'none';
+        buttonMore.textContent = 'Read more';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    buttonMore.addEventListener('click', toggleReadMore)
+})
