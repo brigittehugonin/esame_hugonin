6a08e5f8dd190d31db87c7f410d4480540c01534
Brigitte Hugonin <brigitte.hugonin@gmail.com>
2024-02-01 19:53:11 +0100

    fiorellino

--- script.js
+++ script.js
@@ -66,6 +66,26 @@ const anim = () => {
 
 
 
+// fiorellino
+let nav = document.querySelector('.nav')
+
+let tween = gsap.to(".flair", {
+  duration: 2,
+  x: () => nav.offsetWidth, // animate by the px width of the nav
+  xPercent: -100, // offset by the width of the box
+  rotation: 360,
+  ease: "none",
+  paused: true
+});
+
+// click handlers for controlling the tween instance...
+document.querySelector("#play").onclick = () => tween.play();
+document.querySelector("#pause").onclick = () => tween.pause();
+document.querySelector("#resume").onclick = () => tween.resume();
+document.querySelector("#reverse").onclick = () => tween.reverse();
+document.querySelector("#restart").onclick = () => tween.restart();
+
+
 
 
 
